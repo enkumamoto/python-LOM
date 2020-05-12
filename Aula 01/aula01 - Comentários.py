038f18f8bea72d8536932007e893af6a6adaa77b
Eiji Kumamoto Neto <eiji.kumamoto@gmail.com>
2020-05-12 13:25:38 -0300

    aula01 - Comentários.py
    Teste de uploda

---
+++ Aula 01/aula01 - Comentários.py
@@ -0,0 +1,22 @@
+print ('Linha 1')
+print ('Linha 2')
+# Falando sobre a linha abaixo: está é a linha 3
+# print ('Linha 3')
+print ('Linha 4')  # Isso é um comentário
+print ('Linha 5')
+# print ('Linha 6')
+"""
+print ('Linha 7')
+print ('Linha 8')
+"""
+print ('Linha 9')
+
+# Não faça comentários óbvios
+# Para comentar várias linhas de uma vez abre com 3 aspas duplas (""") ou com 3 aspas simples (''').
+# Ou pode usar da forma abaixo:
+"""
+Texto sobre o programa
+"""
+
+# Explicando
+print (1)
