66782a8445b9a416be16bf32228cd49477e8c28e
Eiji Kumamoto Neto <eiji.kumamoto@gmail.com>
2020-05-14 13:54:14 -0300

    nova versão

--- Aula 31/aula 31.py
+++ Aula 31/aula 31.py
@@ -20,3 +20,55 @@ s1 = {1,2,3,4,5,6}
 for v in s1:
     print (v)
 print ('---')
+
+# não pode criar set vazio, mas caso seja necessário, deve-se fazer da forma abaixo
+s1 = set()
+s1.add(1)  # a função .add adiciona elementos
+s1.add(2)
+s1.add(3)
+s1.add((1,2,3,'Eiji'))
+
+print (s1)
+print ('---')
+
+# descartando elementos do set
+s1 = set()
+s1.add(1)
+s1.add(2)
+s1.add(3)
+s1.add((1,2,3,'Eiji'))
+print (s1)
+
+s1.discard(2)  # a função .discard descarta elementos
+print (s1)
+print ('---')
+
+# a função .update é similar a .add, porem a diferença é que ela é iterável e os elementos não tem posição fixa.
+s1 = set()
+s1.add(1)
+s1.add(2)
+s1.add(3)
+s1.add((1,2,3,'Eiji'))
+print (s1)
+
+s1.discard(2)
+print (s1)
+
+s1.update('Python')
+print (s1)
+print ('---')
+# ainda dentro do update, os sets não aceitam elementos duplicados
+s1 = set()
+s1.update([1,2,3,4,5], {5,6,7,8,9})
+print (s1)
+print ('---')
+
+# uma forma de usar sets é para eliminar elementos repetidos dentro de listas
+l1 = [1,2,3,4,4,3,3,4,3,4,5,6,7,5,5,4,6,4,3,2,2,3,4,5,6,7,8,6,7,5,3,2,4,3,5,7,6,4,3,4, 'eiji', 'Eiji', 'Eiji']
+l1 = set(l1)  # casting lista para set
+print(l1)
+
+l1 = list(l1)  # transformando de set para lista
+print(l1)
+print(l1[-1])
+print ('---')
